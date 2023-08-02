4f71fa02e416ff94fb34787cf507e6af6c25a1f4
whitechopchicken <1249636417@qq.com>
2023-08-02 11:15:08 +0800

    现代C++优化了init(), 还可以尝试用auto关键字

--- workspace/src/logger.cpp
+++ workspace/src/logger.cpp
@@ -65,6 +65,105 @@ bool FileLogAppender::reopen(){
     m_filestream.open(m_filename);
     return !!m_filestream;      //-----转换技巧，转换为bool类的方法！
 }
+LogFormatter::LogFormatter(const std::string& pattern)
+    :m_pattern(pattern){
+}
+std::string LogFormatter::format(LogEvent::ptr event){  //作用：调用“成员类”的同名函数，来对event内容做各种解析
+    std::stringstream ss;
+    for(auto& i : m_items) {        //遍历成员: items数组, 每个item分别调用自己的format()
+        i->format(ss, event);
+    }
+    return ss.str();    //返回解析后的字符串？
+}
+void LogFormatter::init(){
+    //将输入的m_pattern(假设是字符串) 解析成一组格式模板，并将这些模板存储在 容器vec的元组中，每个元组有3个值 string、format、type
+    //我这里用了c++11新特性来优化： 用枚举类表示解析状态，增加代码可读性；用std::string::substr 截取子字符串，代替逐个字符遍历
+    enum ParseStatus{
+        PARSE_TEXT,
+        PARSE_FORMAT
+    };
+    std::vector<std::tuple<std::string, std::string, int> > vec;
+    size_t n = 0;
+    size_t m = m_pattern.size();
+    std::string nstr;
+
+    while(n<m) {                                //解析每个m_pattern的字符
+        char currentChar = m_pattern[n];
+        switch (currentChar)
+        {
+        case '%':   //有%就表示需要解析
+            if((n+1) < m && m_pattern[n+1] == '%') {
+                nstr.append(1, '%');
+                n += 2;
+            }else{
+                if(!nstr.empty()){
+                    vec.push_back({nstr, "", 0});
+                    nstr.clear();
+                }
+
+                ParseStatus ParseStatus = PARSE_TEXT;
+                size_t fmt_begin = 0;
+                std::string str;
+                std::string fmt;
+
+                for(++n; n < m; ++n){
+                    char nextChar = m_pattern[n];
+                    if(ParseStatus == PARSE_TEXT) {
+                        if(nextChar == '{' ) {
+                            str = m_pattern.substr(n-1, fmt_begin-n+1 );
+                            ParseStatus = PARSE_FORMAT;
+                            fmt_begin = n+1;
+                        }
+                    }else if(ParseStatus == PARSE_FORMAT) {
+                        if(nextChar == '}' ) {
+                            fmt = m_pattern.substr(fmt_begin, n - fmt_begin);
+                            ParseStatus = PARSE_TEXT;
+                            ++n;
+                            break;
+                        }
+                    }
+                }
+                if(ParseStatus ==PARSE_TEXT ) {
+                    if(!str.empty()) {
+                        vec.push_back({str, "", 0});
+                    }
+                    vec.push_back({m_pattern.substr(fmt_begin, n-fmt_begin), fmt, 1});
+                }else {
+                    std::cout<<"pattern parse error: "<<m_pattern <<" - " <<m_pattern.substr(n-1) << std::endl;
+                    m_error = true;
+                }
+            }
+            break;
+        
+        default:
+            nstr.append(1, currentChar);
+            ++n;
+            break;
+        }
+    }
+    if(!nstr.empty()){
+        vec.push_back({nstr, "", 0});
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
